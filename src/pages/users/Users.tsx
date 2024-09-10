bf0f1aa7ae63f8e46e68f95582dbe68b84744df2
sayedabdelal <saidmo19734@gmail.com>
2024-09-10 16:52:22 +0200

    dist

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -4,7 +4,7 @@ import "./Users.scss";
 import { useState } from "react";
 import Add from "../../components/add/add";
 import { userRows } from "../../data";
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 60 },
@@ -60,13 +60,13 @@ const Users = () => {
 
   // TEST THE API
 
-  const { isLoading, data } = useQuery({
-    queryKey: ["allusers"],
-    queryFn: () =>
-      fetch("http://localhost:8800/api/users").then(
-        (res) => res.json()
-      ),
-  });
+  // const { isLoading, data } = useQuery({
+  //   queryKey: ["allusers"],
+  //   queryFn: () =>
+  //     fetch("http://localhost:8800/api/users").then(
+  //       (res) => res.json()
+  //     ),
+  // });
 
   return (
     <div className="users">
@@ -74,14 +74,14 @@ const Users = () => {
         <h1>Users</h1>
         <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
-      {/* <DataTable slug="users" columns={columns} rows={userRows} /> */}
+      <DataTable slug="users" columns={columns} rows={userRows} />
       {/* TEST THE API */}
 
-      {isLoading ? (
+      {/* {isLoading ? (
         "Loading..."
       ) : (
         <DataTable slug="users" columns={columns} rows={data} />
-      )}
+      )} */}
       {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   );
