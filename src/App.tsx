6a9cd88d2857852937c759b2618c6f319ea2accc
sayedabdelal <saidmo19734@gmail.com>
2024-09-10 16:40:41 +0200

    General updates and improvements across various pages.

---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+ 
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import Navbar from './components/navbar/Navbar';
+import Menu from './components/menu/Menu';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Users from './pages/users/Users';
+import Products from './pages/products/Products';
+import User from './pages/user/User';
+
+import Product from './pages/product/Product';
+import Login from './pages/login/Login';
+import "./styles/global.scss";
+import EmptyPage from "./pages/empty/EmptyPage";
+
+const queryClient = new QueryClient();
+
+
+
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+ const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "*",
+      element: <EmptyPage />,
+    }
+  ]);
+
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
+
+ 
